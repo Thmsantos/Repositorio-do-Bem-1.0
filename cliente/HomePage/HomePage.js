d3ec75a6bcb9e7a5a8e449c56b677523da732c7b
Thiago Santos <91476713+Thmsantos@users.noreply.github.com>
2024-06-08 18:13:18 -0300

    Add files via upload

---
+++ cliente/HomePage/HomePage.js
@@ -0,0 +1,9 @@
+function PagLogin(event) {
+    window.location.href = '../login/login.html'
+    event.preventDefault();
+}
+
+function PontosColeta(event) {
+    window.location.href = '../Ponto_coleta/ponto_coleta.html'
+    event.preventDefault();
+}
