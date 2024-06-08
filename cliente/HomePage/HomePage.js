ef6010b26c143d0652008a84dd9a2e25b0445c3a
Thiago Santos <91476713+Thmsantos@users.noreply.github.com>
2024-06-08 18:09:36 -0300

    Delete cliente directory

--- cliente/HomePage/HomePage.js
+++
@@ -1,9 +0,0 @@
-function PagLogin(event) {
-    window.location.href = '../login/login.html'
-    event.preventDefault();
-}
-
-function PontosColeta(event) {
-    window.location.href = '../Ponto_coleta/ponto_coleta.html'
-    event.preventDefault();
-}
